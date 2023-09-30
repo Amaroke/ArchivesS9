2c22ff0cd85f95fa9f89b0cae204ffa494b3b96c
Amaroke <hugo.amaroke@gmail.com>
2023-09-30 22:38:00 +0200

    Petit fix

--- Environnement et méthodologie de développement/m2/src/main/java/amaroke/projet_cm/model/entity/LivreEntity.java
+++ Environnement et méthodologie de développement/m2/src/main/java/amaroke/projet_cm/model/entity/LivreEntity.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,7 +32,7 @@ public class LivreEntity {
     @JsonProperty("titre")
     private String titre;
 
-    @ManyToMany(mappedBy = "livres")
+    @OneToMany(mappedBy = "livre")
     @JsonProperty("bibliotheques")
     List<BiblioJoinLivreEntity> biblioJoinLivreEntities;
 
