5024ff5ef50f5368a92e0aef556f4ebec3e9786e
Amaroke <hugo.amaroke@gmail.com>
2024-02-26 16:03:57 +0100

    Reducer mis à jour début de défi 1

--- Hadoop/Projet/src/reducers/TaskReducer.java
+++ Hadoop/Projet/src/reducers/TaskReducer.java
@@ -1,38 +1,43 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class TaskReducer extends Reducer<Text, IntWritable, Text, Text> {
-
+    
     @Override
-    public void reduce(Text key, Iterable<IntWritable> values, Context context)
-            throws IOException, InterruptedException {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         int totalDuration = 0;
         int instanceCount = 0;
         int stragglerCount = 0;
-        double averageDuration;
+        ArrayList<Integer> durations = new ArrayList<>();
 
-        // Calcul de la durée totale et du nombre d'instances
+        // Calcul de la durée totale et du nombre d'instances, stockage des durées
         for (IntWritable value : values) {
             int duration = value.get();
             totalDuration += duration;
             instanceCount++;
+            durations.add(duration);
         }
 
         // Calcul de la durée moyenne
-        averageDuration = (double) totalDuration / instanceCount;
+        double averageDuration = (double) totalDuration / instanceCount;
 
-        // Reset pour calculer le nombre de stragglers
-        for (IntWritable value : values) {
-            if (value.get() > averageDuration * 1.20) { // 20% plus long que la moyenne
+        // Calcul du nombre de stragglers
+        for (int duration : durations) {
+            if (duration > averageDuration * 1.20) { // 20% plus long que la moyenne
                 stragglerCount++;
             }
         }
 
+        // Afficher des informations résumées pour le débogage
+        if (stragglerCount > 0 || instanceCount > 1000) { // Afficher seulement pour les tâches significatives
+            System.out.println("Tâche: " + key.toString() + ", Stragglers: " + stragglerCount + ", Instances: " + instanceCount);
+        }
+
         // Écrire le résultat pour la tâche
-        String result = "Durée Totale: " + totalDuration + ", Nombre d'Instances: " + instanceCount + ", Stragglers: "
-                + stragglerCount;
+        String result = "Durée Totale: " + totalDuration + ", Nombre d'Instances: " + instanceCount + ", Stragglers: " + stragglerCount;
         context.write(key, new Text(result));
     }
 }
